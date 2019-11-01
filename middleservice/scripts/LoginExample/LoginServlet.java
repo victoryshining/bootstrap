d36744d68d5fd6fadd7808ee877d6603a3304e80
sheng <36556285+victoryshining@users.noreply.github.com>
2019-11-01 16:33:32 +0800

    Add files via upload

---
+++ middleservice/scripts/LoginExample/LoginServlet.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+
+
+public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			           throws ServletException, java.io.IOException {
+
+try
+{	    
+
+     String user = request.getParameter("un");
+     String passwd = request.getParameter("pw");
+
+    
+	   		    
+     if (user.equals(passwd))
+     {
+	        
+          HttpSession session = request.getSession(true);	    
+          session.setAttribute("currentSessionUser",user); 
+          response.sendRedirect("userLogged.jsp"); //logged-in page      		
+     }	        
+     else 
+          response.sendRedirect("invalidLogin.jsp"); //error page 
+} 
+		
+		
+catch (Throwable theException) 	    
+{
+     System.out.println(theException); 
+}
+       }
+}
---
+++ middleservice/scripts/LoginExample/userLogged.jsp
@@ -0,0 +1,27 @@
+ <%@ page language="java" 
+         contentType="text/html; charset=windows-1256"
+         pageEncoding="windows-1256"
+
+   %>
+ 
+   <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+   <html>
+
+      <head>
+         <meta http-equiv="Content-Type" 
+            content="text/html; charset=windows-1256">
+         <title>   User Logged Successfully   </title>
+      </head>
+	
+      <body>
+
+         <center>
+           			
+            Welcome you!
+         </center>
+
+      </body>
+	
+   </html>
